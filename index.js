0544c21f4430b6f35cb6c031eef21fb3bd4b3483
Prashant Kumar <39859920+prashant-08@users.noreply.github.com>
2025-04-16 10:12:49 +0530

    Add: full stack open backend folder for deployment on render

---
+++ index.js
@@ -0,0 +1,158 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+let notes = [
+    {
+      id: "1",
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: "2",
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: "3",
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+]
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => Number(n.id))) : 0
+  return String(maxId)
+}
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId()
+  }
+  notes = notes.concat(note)
+  console.log(note)
+  response.json(note)
+})
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  console.log('request came here in delete')
+  const id = request.params.id
+  console.log(id);
+  notes = notes.filter(note => note.id !== id)
+  console.log(notes)
+  response.status(204).end()
+})
+
+/*
+const getRandomId = () => {
+  const id = Math.floor(Math.random()*1000000)
+  return String(id)
+}
+
+let persons = [
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(204).end()
+  }
+})
+
+app.get('/info', (request, response) => {
+  response.send(`Phonebook has info for ${persons.length} people
+     ${new Date()}`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  console.log(persons)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, resposne) => {
+  const person = request.body
+  if (!person.name || person.number) {
+    return resposne.status(400).json({
+      error: "either name or number is missing"
+    })
+  }
+
+  if (persons.find(p => p.name === person.name)) {
+    return resposne.status(400).json({
+      error: "name must be unique"
+    })
+  }
+  person.id = getRandomId()
+  persons = persons.concat(person)
+  resposne.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+*/
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=> {
+    console.log(`Server running on port ${PORT}`)
+})
