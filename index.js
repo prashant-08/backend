e12a9f006cbd4a92fc8377476b69351dd0afe249
prashant-08 <kumarprashant.iitg.6032@gmail.com>
2025-04-23 11:47:31 +0530

    backend project structuring and writing basics testcases

--- index.js
+++ index.js
@@ -1,87 +1,14 @@
-require('dotenv').config()
-const express = require('express')
-const Note = require('./models/note')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
 // const Phonebook = require('./models/phonebook')
-
-
-const app = express()
-
-app.use(express.json())
-app.use(express.static('dist'))
-
-app.post('/api/notes', (request, response, next) => {
-  const body = request.body
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  })
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  }).catch(error => next(error))
-})
+/*
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-  Note.findById(request.params.id).then(note => {
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  }).catch (error => {
-    next(error)
-  })
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-  const { content, important } = request.body
-
-  Note.findById(request.params.id)
-    .then(note => {
-      if(!note) {
-        return response.status(404).end()
-      }
-      note.content = content
-      note.important = important
-
-      return note.save().then((updateNote) => {
-        response.json(updateNote)
-      }).catch(error => next(error))
-    })
-})
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-  next(error)
-}
-
-app.use(errorHandler)
-
-/*
 app.get('/api/persons', (request, response) => {
   Phonebook.find({}).then((phonebooks) => {
     response.json(phonebooks)
@@ -147,26 +74,8 @@ app.put('/api/persons/:id', (request, response, next) => {
     })
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-  next(error)
-}
-
-app.use(errorHandler)
 */
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URI
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  }).catch(error => {
-    console.log('error connecting to mongodb', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
